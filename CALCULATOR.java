c4e300ae96ff6a0ba3461be196753620c3796896
Dela <dela.acolatse@ashesi.edu.gh>
2017-01-22 20:29:21 +0000

    Done with addition

---
+++ CALCULATOR.java
@@ -0,0 +1,13 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class CALCULATOR {
+  
+  public static void main (String[] args){
+    
+ if(args[0].equals("add")){
+   double x = Double.parseDouble(args[1]);
+   double y = Double.parseDouble(args[2]);
+     System.out.println(x+y);
+ }  
+}
